ff1883ba032b9962342275780d3ea0eea3d54b34
kaseris <mich.kaseris@gmail.com>
2020-11-25 13:08:54 +0200

    results paths

--- evaluation/check_fscores_summe_with_gts.py
+++ evaluation/check_fscores_summe_with_gts.py
@@ -5,7 +5,7 @@ import h5py
 from generate_summary import generate_summary
 from evaluation_metrics import evaluate_summary
 
-path = '../exp/SumMe/results/split1' # path to the json files with the computed importance scores for each epoch
+path = '../model/exp0/TVSum/results/split0' # path to the json files with the computed importance scores for each epoch
 results = listdir(path)
 results.sort(key=lambda video: int(video[6:-5]))
 PATH_SumMe = '../data/SumMe/eccv16_dataset_summe_google_pool5.h5'
@@ -27,8 +27,8 @@ for epoch in results:
     all_gt_scores, all_shot_bound, all_nframes, all_positions = [], [], [], []
     with h5py.File(PATH_SumMe, 'r') as hdf:
         for video_name in keys:
-            video_index = video_name[6:] 
-            
+            video_index = video_name[6:]
+
             # Using ground truth scores instead of user summary
             gt_score = np.array( hdf.get('video_'+video_index+'/gtscore') )
             sb = np.array( hdf.get('video_'+video_index+'/change_points') )
@@ -42,7 +42,7 @@ for epoch in results:
 
     # generate summary based on the auto-generated scores
     all_summaries = generate_summary(all_shot_bound, all_scores, all_nframes, all_positions)
-    
+
     # generate summary based on the ground-truth scores
     all_gt_summary = generate_summary(all_shot_bound, all_gt_scores, all_nframes, all_positions)
 
@@ -51,11 +51,11 @@ for epoch in results:
     for video_index in range(len(all_summaries)):
         summary = all_summaries[video_index]
         gt_summary = all_gt_summary[video_index]
-        f_score = evaluate_summary(summary, np.expand_dims(gt_summary, axis=0), eval_method)	
+        f_score = evaluate_summary(summary, np.expand_dims(gt_summary, axis=0), eval_method)
         all_f_scores.append(f_score)
 
     f_score_epochs.append(np.mean(all_f_scores))
     print("f_score: ",np.mean(all_f_scores))
 
-with open(path+'/f_scores.txt', 'w') as outfile:  
+with open(path+'/f_scores.txt', 'w') as outfile:
     json.dump(f_score_epochs, outfile)
--- evaluation/check_fscores_tvsum.py
+++ evaluation/check_fscores_tvsum.py
@@ -5,7 +5,7 @@ import h5py
 from generate_summary import generate_summary
 from evaluation_metrics import evaluate_summary
 
-path = '../exp/TVSum/results/split1' # path to the json files with the computed importance scores for each epoch
+path = '../model/exp0/TVSum/results/split0' # path to the json files with the computed importance scores for each epoch
 results = listdir(path)
 results.sort(key=lambda video: int(video[6:-5]))
 PATH_TVSum = '../data/TVSum/eccv16_dataset_tvsum_google_pool5.h5'
@@ -25,10 +25,10 @@ for epoch in results:
             all_scores.append(scores)
 
     all_user_summary, all_shot_bound, all_nframes, all_positions = [], [], [], []
-    with h5py.File(PATH_TVSum, 'r') as hdf:        
+    with h5py.File(PATH_TVSum, 'r') as hdf:
         for video_name in keys:
             video_index = video_name[6:]
-            
+
             user_summary = np.array( hdf.get('video_'+video_index+'/user_summary') )
             sb = np.array( hdf.get('video_'+video_index+'/change_points') )
             n_frames = np.array( hdf.get('video_'+video_index+'/n_frames') )
@@ -46,11 +46,11 @@ for epoch in results:
     for video_index in range(len(all_summaries)):
         summary = all_summaries[video_index]
         user_summary = all_user_summary[video_index]
-        f_score = evaluate_summary(summary, user_summary, eval_method)	
+        f_score = evaluate_summary(summary, user_summary, eval_method)
         all_f_scores.append(f_score)
 
     f_score_epochs.append(np.mean(all_f_scores))
     print("f_score: ",np.mean(all_f_scores))
 
-with open(path+'/f_scores.txt', 'w') as outfile:  
+with open(path+'/f_scores.txt', 'w') as outfile:
     json.dump(f_score_epochs, outfile)
